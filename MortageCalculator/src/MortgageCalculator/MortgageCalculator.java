4311fc26c35365d68d81bfde9fe4434b4314ef86
Fred Xie <Fred@Freds-MBP.home>
2015-10-22 03:14:29 -0400

    NA-0: create repo and push

---
+++ MortageCalculator/src/MortgageCalculator/MortgageCalculator.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MortgageCalculator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Fred
+ */
+public class MortgageCalculator {
+
+    public static List<MortgageParameter> mortgageParams = new ArrayList<>();
+    public static final String DELIMITER = "-----------------------------";
+    
+    /**
+     * 
+     * @param args
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws Exception {
+        MortgageCalculator calculator = new MortgageCalculator();
+        calculator.run(args);
+    }
+    
+    public void run (String[] args) throws Exception
+    {
+        GettingCalculationInputFromConsole();
+        CalculateData();
+    }
+    
+    /**
+     * 
+     * @throws Exception 
+     */
+    private void GettingCalculationInputFromConsole() throws Exception {
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome to mortage calculator");
+        PrintDelimiter();
+        System.out.println("Please input total price (in unit of $10,000)");
+        double totalPrice = 10000 * Double.parseDouble(reader.readLine());
+        PrintDelimiter();
+        System.out.println("Please input down payment (in unit of $10,000)");
+        double downPayment = 10000 * Double.parseDouble(reader.readLine());
+        PrintDelimiter();
+        System.out.println("Please input loan length (in year)");
+        int loanMonthLength = 12 * Integer.parseInt(reader.readLine());
+        PrintDelimiter();
+        System.out.println("Please input APR, e.g. 5% please input 5");
+        System.out.println("If you want to compare multiple APR, input APRs with ';' in between");
+        List<Double> aprs = Arrays.asList(reader.readLine().split(";"))
+                .stream()
+                .map(apr -> {return Double.parseDouble(apr)/(double)100;})
+                .collect(Collectors.toList());
+        PrintDelimiter();
+        System.out.println("Please input closing cost (in unit of $10,000)");
+        List<Double> ccs = Arrays.asList(reader.readLine().split(";"))
+                .stream()
+                .map(cc -> {return 10000 * Double.parseDouble(cc);})
+                .collect(Collectors.toList());
+        PrintDelimiter();
+        System.out.println("Please input expected owning length before selling (in year)");
+        int ownMonthLength = 12 * Integer.parseInt(reader.readLine());
+
+        aprs.forEach(
+                apr -> {
+                    mortgageParams.add(
+                            new MortgageParameter(totalPrice, downPayment, loanMonthLength, apr, ccs.get(aprs.indexOf(apr)), ownMonthLength)
+                    );
+                }
+        );
+    }
+    
+    private void CalculateData() throws Exception {
+        mortgageParams.forEach(param -> { 
+            param.PopulateMortgageParameters();
+            SumAndPrint(param);
+        });
+        
+    }
+    
+    private static void SumAndPrint(MortgageParameter param) {
+        double totalPaymentToDate = param.closingCost + param.downPayment + param.monthlyPayment * param.ownMonthLength;
+        double totalPrincipalPaymentToDate = param.GetPrincipalPaymentSum();
+        double totalInterestPaymentToDate = param.GetInterestPaymentSum();
+        double totalPrincipalLeft = param.totalPrice - param.downPayment - totalPrincipalPaymentToDate;
+        double owningCost = totalPaymentToDate - (param.totalPrice - totalPrincipalLeft);
+        
+        PrintDelimiter();
+        System.out.println("APR Rate: " + new DecimalFormat("##.####").format(param.apr));
+        System.out.println("Total Paid: " + new DecimalFormat("##.##").format(totalPaymentToDate));
+        System.out.println("  -- Down Payment: " + new DecimalFormat("##.##").format(param.downPayment));
+        System.out.println("  -- Total Principal: " + new DecimalFormat("##.##").format(totalPrincipalPaymentToDate));
+        System.out.println("  -- Total Interest: " + new DecimalFormat("##.##").format(totalInterestPaymentToDate));
+        System.out.println("Total Principal left: " + new DecimalFormat("##.##").format(totalPrincipalLeft));
+        System.out.println("Assume selling at purchase price, owning cost: " + new DecimalFormat("##.##").format(owningCost));
+        PrintDelimiter();
+    }
+    
+    private static void PrintDelimiter() {
+        System.out.println(DELIMITER);
+    }
+}
---
+++ MortageCalculator/src/MortgageCalculator/MortgageParameter.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MortgageCalculator;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import static MortgageCalculator.MortgageCalculator.DELIMITER;
+
+/**
+ *
+ * @author Fred
+ */
+public class MortgageParameter {
+    public double totalPrice;
+    public double downPayment;
+    public double mortgageAmount;
+    public int loanMonthLength;
+
+    public double apr;
+    public double monthlyInterestRate;
+    public double closingCost;
+    public int ownMonthLength;
+
+    //mortgage related params
+    public double monthlyPayment;
+    public HashMap<Integer, Double> interestPaymentMap;
+    public HashMap<Integer, Double> principalPaymentMap;
+
+    public MortgageParameter(double tp, double dp, int lml, double a, double cc, int oml) {
+        totalPrice = tp;
+        downPayment = dp;
+        mortgageAmount = tp - dp;
+        loanMonthLength = lml;
+        apr = a;
+        monthlyInterestRate = a/(double)12;
+        closingCost = cc;
+        ownMonthLength = oml;
+
+        monthlyPayment = 0;
+        interestPaymentMap = new HashMap<>();
+        principalPaymentMap = new HashMap<>();
+    }
+
+    public void PopulateMortgageParameters() {
+        PopulateMonthlyPayment();
+        PopulateInterestPaymentMap();
+        PopulatePrincipalPaymentMap();
+    }
+
+    public void PopulateMonthlyPayment() {
+        monthlyPayment = (mortgageAmount * monthlyInterestRate * Math.pow((monthlyInterestRate + 1), loanMonthLength)) / (Math.pow((monthlyInterestRate + 1), loanMonthLength) - 1);
+//        System.out.println("ESTIMATED MONTHLY PAYMENT: " + new DecimalFormat("##.##").format(monthlyPayment));
+    }
+
+    public void PopulateInterestPaymentMap() {
+//        PrintDelimiter();
+//        System.out.println("MONTH   |   INTEREST PAYMENT");
+        for (int month = 1; month <= loanMonthLength; month++) {
+            if (!interestPaymentMap.containsKey(month)) {
+                double previousTotalInterestPayment = GetInterestPaymentSum(1, month);
+                double currentMonthInterestPayment = (mortgageAmount - (month - 1) * monthlyPayment + previousTotalInterestPayment) * monthlyInterestRate;
+
+                interestPaymentMap.put(month, currentMonthInterestPayment);
+//                System.out.println(month + "   |   $" + new DecimalFormat("##.##").format(currentMonthInterestPayment));
+            }
+        }
+
+    }
+
+    public void PopulatePrincipalPaymentMap() {
+//        PrintDelimiter();
+//        System.out.println("MONTH   |   PRINCIPAL PAYMENT");
+        for (int month = 1; month <= loanMonthLength; month++) {
+            if (!principalPaymentMap.containsKey(month)) {
+                double currentMonthInterestPayment = interestPaymentMap.containsKey(month) ? interestPaymentMap.get(month) : 0;
+                double currentPrincipalPayment = monthlyPayment - currentMonthInterestPayment;
+
+                principalPaymentMap.put(month, currentPrincipalPayment);
+//                System.out.println(month + "   |   $" + new DecimalFormat("##.##").format(currentPrincipalPayment));
+            }
+        }
+    }
+    
+    public double GetInterestPaymentSum() {
+        return GetInterestPaymentSum(1, this.ownMonthLength);
+    }
+    
+    public double GetPrincipalPaymentSum() {
+        return GetPrincipalPaymentSum(1, this.ownMonthLength);
+    }
+    
+    /**
+     * 
+     * @param startMonth
+     * @param endMonth
+     * @return summedInterestPayment
+     */
+    private double GetInterestPaymentSum(int startMonth, int endMonth) {
+        double totalInterestPayment = 0;
+        
+        totalInterestPayment = interestPaymentMap.entrySet().stream()
+                .filter((entry) -> (entry.getKey() >= startMonth && entry.getKey() <= endMonth))
+                .map((entry) -> entry.getValue()).reduce(totalInterestPayment, (accumulator, _item) -> accumulator + _item);
+        
+        return totalInterestPayment;
+    }
+    
+    /**
+     * 
+     * @param startMonth
+     * @param endMonth
+     * @return 
+     */
+    private double GetPrincipalPaymentSum(int startMonth, int endMonth) {
+        double totalPrincipalPayment = 0;
+        
+        totalPrincipalPayment = principalPaymentMap.entrySet().stream()
+                .filter((entry) -> (entry.getKey() >= startMonth && entry.getKey() <= endMonth))
+                .map((entry) -> entry.getValue()).reduce(totalPrincipalPayment, (accumulator, _item) -> accumulator + _item);
+        
+        return totalPrincipalPayment;
+    }
+    
+    private static void PrintDelimiter() {
+        System.out.println(DELIMITER);
+    }
+}
